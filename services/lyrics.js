deec78b6fafedd250b86de7963715dbcf8e48b30
Rohan <rohanrc101@gmail.com>
2024-12-31 23:56:27 +0300

    [DOCS] Function documentation

--- services/lyrics.js
+++ services/lyrics.js
@@ -3,9 +3,10 @@ const { readFileLyrics } = require("../utils/fileHandler")
 
 module.exports = {
     /**
-        * 
+        * This function retrieves lyrics from a specified file and returns the lyrics of the song with the given ID.
         * @param {*} songType The type of the song to search for (e.g, ffpm ,Fihirana fanampiny, Antema)
         * @param {*} songId The id of the song to search for
+        * @returns An object containing the requested song
         */
     async retrieveLyrics(songType, songId) {
         try {
@@ -28,12 +29,19 @@ module.exports = {
 
             const key = `${fileName}_${songId}`
 
+            if (!jsonData[key]) {
+                throw new Error(`Song ID "${songId}" not found in file "${fileName}"`)
+            }
+
             const jsonData = JSON.parse(data)
 
             const result = jsonData[key].hira
+            return result
+
         }
         catch (error) {
             console.log("An error has occured :", error)
+            throw new Error("An error occurred in processing the song data: " + error.message)
         }
 
 
