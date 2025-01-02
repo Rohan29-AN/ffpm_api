fbd606200e7536c5c2d19cc6f74859ea145f0be5
Rohan <rohanrc101@gmail.com>
2025-01-02 20:15:38 +0300

    [FEAT] route configuration

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { getLyrics } = require('../controllers/fihiranaController');
 const router = express.Router()
 
+router.get('/lyrics/:songId',getLyrics)
 
 module.exports = router
