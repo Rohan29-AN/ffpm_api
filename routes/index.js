dc1c80fbbab339d40b208559d300b0dbd0abea66
Rohan <rohanrc101@gmail.com>
2025-01-03 21:22:45 +0300

    [FEAT] definition of the verse schema

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,26 @@ const express = require('express');
 const { getLyrics } = require('../controllers/fihiranaController');
 const router = express.Router()
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Verse:
+ *       type:  object
+ *       properties:
+ *         andininy:
+ *           type:  integer
+ *           description:  The verse ID
+ *         tononkira:
+ *           type:  string
+ *           description:  The verse
+ *         fiverenany:
+ *           type:  boolean
+ *           description:  Variable to indicate whether it is the chorus or not
+ *        
+ */
+
+
 router.get('/lyrics/:songId',getLyrics)
 
 module.exports = router
